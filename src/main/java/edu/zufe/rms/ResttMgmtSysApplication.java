7c7ee43750304d9d5bfe9c884af45b44c507bf17
caoliangkun <caoliangkun@outlook.com>
2018-04-03 08:36:40 +0800

    initial commit

---
+++ src/main/java/edu/zufe/rms/ResttMgmtSysApplication.java
@@ -0,0 +1,12 @@
+package edu.zufe.rms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResttMgmtSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResttMgmtSysApplication.class, args);
+	}
+}
---
+++ src/test/java/edu/zufe/restt_mgmt_sys/ResttMgmtSysApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.zufe.restt_mgmt_sys;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ResttMgmtSysApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
